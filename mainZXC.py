0b01948026e6dff48ee5d9c9cd4d2387c92cc7c8
unknown <you@example.com>
2024-11-11 10:50:13 +0300

    python script 0.3

--- mainZXC.py
+++ mainZXC.py
@@ -1,6 +1,15 @@
-print("Меню:")
-print("1. Пункт 1")
-print("2. Пункт 2")
-print("3. Пункт 3")
+def option_one():
+    print("пункт 1!")
+
+def option_two():
+    print("пункт 2!")
+
+def option_three():
+    print("пункт 3!")
+
+option_one()
+option_two()
+option_three()
+print("пункт 4!")
 
 # by atari :)
